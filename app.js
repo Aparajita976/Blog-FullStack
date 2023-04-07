e277f17240f39d62b3f7fd93474ead11c4303574
Aparajita Bandyopadhyay <117743177+Aparajita976@users.noreply.github.com>
2023-04-07 09:56:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,26 @@ import path from 'path';
 const __dirname = path.resolve();
 //const __filename = fileURLToPath(import.meta.url);
 //const __dirname = dirname(__filename);
-//import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 const app = express();
 
 import userRouter from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+app.use(cors());
+//app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, './frontend/build')))
-//app.use(cors);
+
 app.use("/user", userRouter);
 app.use("/blog", blogRouter);
-//dotenv.config();
+dotenv.config();
 mongoose.connect('mongodb+srv://admin:lTgFmfLTH7RrdNYe@cluster0.4ruf5ba.mongodb.net/?retryWrites=true&w=majority').then(() => console.log("DBconnection successfull")).catch((error) => { console.log("error") });
 app.use('*', function (req, res) {
     res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 })
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log("server is running");
 })
 
